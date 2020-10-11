edf63353af57d026885e90dc808e707ad21ad62b
sandeep <sandeep.gantait@sap.com>
2020-10-11 21:34:27 +0530

    Updates

---
+++ precache-manifest.9c652d765065847214b8dd93b4106098.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8546b09f5b20397467c9b438b95f2c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "babd926e220aa40356dd",
+    "url": "/static/css/main.db0f7df4.chunk.css"
+  },
+  {
+    "revision": "e8c2aa68e1d3d91ef6bb",
+    "url": "/static/js/0.d6b39c3d.chunk.js"
+  },
+  {
+    "revision": "2402733240856fa9085a",
+    "url": "/static/js/3.16c680f3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/3.16c680f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69fddae9d9a190da8482",
+    "url": "/static/js/4.b082da23.chunk.js"
+  },
+  {
+    "revision": "16c8be6c2d8ab582edfa",
+    "url": "/static/js/5.3ab1fb56.chunk.js"
+  },
+  {
+    "revision": "e9156b4c733ebb4f37f1",
+    "url": "/static/js/6.748212a3.chunk.js"
+  },
+  {
+    "revision": "babd926e220aa40356dd",
+    "url": "/static/js/main.bbee7b38.chunk.js"
+  },
+  {
+    "revision": "2b3aabd966adc05225e6",
+    "url": "/static/js/runtime-main.37eee75d.js"
+  }
+]);
