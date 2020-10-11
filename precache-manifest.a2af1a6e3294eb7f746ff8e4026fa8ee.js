8983d5506a5c19a62cb9e19eba2e12d8f80d8693
sandeep <sandeep.gantait@sap.com>
2020-10-11 21:39:49 +0530

    Updates

---
+++ precache-manifest.a2af1a6e3294eb7f746ff8e4026fa8ee.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525a448ceeff1d5f347a09ef777349a7",
+    "url": "/repo-browser/index.html"
+  },
+  {
+    "revision": "e349f7d57a78f8184c1d",
+    "url": "/repo-browser/static/css/main.db0f7df4.chunk.css"
+  },
+  {
+    "revision": "e8c2aa68e1d3d91ef6bb",
+    "url": "/repo-browser/static/js/0.d6b39c3d.chunk.js"
+  },
+  {
+    "revision": "d3ffac4de98da33c522f",
+    "url": "/repo-browser/static/js/3.a1ecdb81.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/repo-browser/static/js/3.a1ecdb81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69fddae9d9a190da8482",
+    "url": "/repo-browser/static/js/4.b082da23.chunk.js"
+  },
+  {
+    "revision": "16c8be6c2d8ab582edfa",
+    "url": "/repo-browser/static/js/5.3ab1fb56.chunk.js"
+  },
+  {
+    "revision": "e9156b4c733ebb4f37f1",
+    "url": "/repo-browser/static/js/6.748212a3.chunk.js"
+  },
+  {
+    "revision": "e349f7d57a78f8184c1d",
+    "url": "/repo-browser/static/js/main.a4af7ab0.chunk.js"
+  },
+  {
+    "revision": "c818afa497bc599a2d99",
+    "url": "/repo-browser/static/js/runtime-main.69620c5d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c652d765065847214b8dd93b4106098.js"
+  "/repo-browser/precache-manifest.a2af1a6e3294eb7f746ff8e4026fa8ee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/repo-browser/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
